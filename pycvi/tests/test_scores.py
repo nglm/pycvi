210600e1831c132cf5d56f306f631a5d33bcaa9e
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-08-23 16:09:57 +0200

    testing basic score functions

--- pycvi/tests/test_scores.py
+++ pycvi/tests/test_scores.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy.testing import assert_array_equal
+from sklearn.cluster import KMeans
 
+from ..datasets import mini
 from ..compute_scores import (
     better_score, argbest, best_score, argworst, worst_score,
-    compute_score
+    compute_score, f_cdist, f_pdist, f_intra, f_inertia, compute_all_scores,
 )
 
 def test_comparisons():
@@ -42,5 +44,80 @@ def test_comparisons():
         msg = "best_score was wrong,      i: {} | s1: {} | s2 {}".format(i, s1, s2)
         assert (out == out_exp_score), msg
 
+def test_f_pdist():
+    for mulivariate in [True, False]:
+        data, time = mini(multivariate=mulivariate)
+        (N, T, d) = data.shape
+        # DTW case
+        dist = f_pdist(data)
+        assert type(dist) == np.ndarray
+
+        # Non DTW case
+        data = data.reshape(N, -1)
+        dist = f_pdist(data)
+        assert type(dist) == np.ndarray
+
+
+def test_f_cdist():
+    for mulivariate in [True, False]:
+        data, time = mini(multivariate=mulivariate)
+        (N, T, d) = data.shape
+        # DTW case
+        dist = f_cdist(data[N//2:], data[:N//2])
+        assert type(dist) == np.ndarray
+        exp_shape = (N-N//2, N//2)
+        assert dist.shape == exp_shape
+
+        # Non DTW case
+        data = data.reshape(N, -1)
+        dist = f_cdist(data[N//2:], data[:N//2])
+        assert type(dist) == np.ndarray
+        exp_shape = (N-N//2, N//2)
+        assert dist.shape == exp_shape
+
+def test_f_intra():
+    for mulivariate in [True, False]:
+        data, time = mini(multivariate=mulivariate)
+        (N, T, d) = data.shape
+        # DTW case
+        dist = f_intra(data)
+        assert type(dist) == float
+
+        # Non DTW case
+        data = data.reshape(N, -1)
+        dist = f_intra(data)
+        assert type(dist) == float
+
+def test_f_inertia():
+    for mulivariate in [True, False]:
+        data, time = mini(multivariate=mulivariate)
+        (N, T, d) = data.shape
+        # DTW case
+        dist = f_inertia(data)
+        assert type(dist) == float
+
+        # Non DTW case
+        data = data.reshape(N, -1)
+        dist = f_inertia(data)
+        assert type(dist) == float
+
+def test_compute_subscores():
+    pass
+
 def test_compute_score():
+    pass
+    # Test with DTW and sliding window
+    # data_clus is a list of T (N, w_t, d) arrays
+
+
+    # Test with DTW but not sliding window
+    # data_clus is a list of 1 (N, T, d) array
+
+    # Test without DTW but with sliding window
+    # data_clus is a list of T (N, w_t*d) arrays
+
+    # Test without DTW nor sliding window
+    # data_clus is a list of 1 (N, T*d) array
+
+def test_compute_all_scores():
     pass
