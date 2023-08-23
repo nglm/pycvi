bc283cd762dadb30e910a97692497ea6bf206c43
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-08-23 14:12:30 +0200

    add datasets

---
+++ pycvi/datasets/__init__.py
@@ -0,0 +1,12 @@
+"""
+Some toy datasets for examples and tests
+
+Datasets should illustrate the impact of different clustering scores,
+different metrics and different clustering algorithm. Some should also
+be representative of a typical real life application
+"""
+
+# To replace 'from .mini import mini'
+# And use from . import mini'
+from .mini import mini
+
