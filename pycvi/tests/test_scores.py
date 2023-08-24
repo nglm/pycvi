930f6c288570194ada5aab23d761be6338272bcd
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-08-24 10:40:58 +0200

    testing test_compute_score

--- pycvi/tests/test_scores.py
+++ pycvi/tests/test_scores.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 from sklearn.cluster import KMeans
+from ..scores import Inertia
 
 from ..datasets import mini
 from ..compute_scores import (
@@ -8,6 +9,7 @@ from ..compute_scores import (
     compute_score, f_cdist, f_pdist, f_intra, f_inertia, compute_all_scores,
     compute_subscores
 )
+from ..cvi import silhouette
 
 def test_comparisons():
     maximize = False
@@ -119,6 +121,7 @@ def test_compute_subscores():
         assert type(dist[0]) == float
 
         # Non DTW case
+        data = data.reshape(N, -1)
         dist = compute_subscores("inertia", data, [c1, c2], "inertia", f_inertia)
         assert type(dist) == float
         dist = compute_subscores("max_inertia", data, [c1, c2], "inertia", f_inertia)
@@ -128,19 +131,41 @@ def test_compute_subscores():
         assert type(dist[0]) == float
 
 def test_compute_score():
-    pass
-    # Test with DTW and sliding window
-    # data_clus is a list of T (N, w_t, d) arrays
+    """
+    Test types of outputs and try with "str", "Score" and "callable" scores
+    """
+    for mulivariate in [True, False]:
+        data, time = mini(multivariate=mulivariate)
+        (N, T, d) = data.shape
+        c1 = [i for i in range(N//2)]
+        c2 = [i for i in range(N//2, N)]
 
+        # DTW case
+        dist = compute_score("inertia", data, [c1, c2])
+        assert type(dist) == float
+        dist = compute_score("max_inertia", data, [c1, c2])
+        assert type(dist) == float
+        dist = compute_score("list_inertia", data, [c1, c2])
+        assert type(dist) == list
+        assert type(dist[0]) == float
+        dist = compute_score(Inertia, data, [c1, c2])
+        assert type(dist) == float
+        dist = compute_score(silhouette, data, [c1, c2])
+        assert type(dist) == float
 
-    # Test with DTW but not sliding window
-    # data_clus is a list of 1 (N, T, d) array
-
-    # Test without DTW but with sliding window
-    # data_clus is a list of T (N, w_t*d) arrays
-
-    # Test without DTW nor sliding window
-    # data_clus is a list of 1 (N, T*d) array
+        # Non DTW case
+        data = data.reshape(N, -1)
+        dist = compute_score("inertia", data, [c1, c2])
+        assert type(dist) == float
+        dist = compute_score("max_inertia", data, [c1, c2])
+        assert type(dist) == float
+        dist = compute_score("list_inertia", data, [c1, c2])
+        assert type(dist) == list
+        assert type(dist[0]) == float
+        dist = compute_score(Inertia, data, [c1, c2])
+        assert type(dist) == float
+        dist = compute_score(silhouette, data, [c1, c2])
+        assert type(dist) == float
 
 def test_compute_all_scores():
     pass
