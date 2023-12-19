34d6fb1d1ca17961ea471d93e7e2a38f7c802d57
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-12-19 10:26:16 +0100

    switching to aeon instead of tslearn

--- examples/clustering.py
+++ examples/clustering.py
@@ -4,7 +4,8 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering, KMeans, SpectralClustering
 from sklearn.preprocessing import StandardScaler
 from sklearn_extra.cluster import KMedoids
-from tslearn.clustering import TimeSeriesKMeans
+# from tslearn.clustering import TimeSeriesKMeans
+from aeon.clustering import TimeSeriesKMeans
 import time
 import matplotlib.pyplot as plt
 from typing import List, Sequence, Tuple
