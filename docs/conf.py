167350fa284dc2b24bae67d5d351e82765a6cb80
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-12-20 19:42:18 +0100

    mock import for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,10 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 # Because tslearn and aeon are not among the standard libraries
 # And we don't want to document tests.
-autodoc_mock_imports = ["aeon", "numpy", "pycvi.tests"]
+autodoc_mock_imports = [
+    "aeon", "numpy", "sklearn",
+    "pycvi.tests"
+]
 
 # -- Project information -----------------------------------------------------
 
