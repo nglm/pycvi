67584dde0cedc7714e194ae952d369603470422c
Natacha, Galmiche <natacha.galmiche@uib.no>
2024-09-24 17:11:50 +0200

    release 0.1.4

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2023, Natacha Galmiche'
 author = 'Natacha Galmiche'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
